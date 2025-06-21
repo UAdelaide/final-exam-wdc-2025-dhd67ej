cb08dc9c58f38a2e0c5a7c153ce65c3c654700e6
Deze Yang <1198401441@qq.com>
2025-06-21 11:25:49 +0930

    Update app.js

--- part2/app.js
+++ part2/app.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
 
-// Routes
+app.use(session({
+  secret: 'mySecret', // 可以使用 process.env.SECRET 更安全
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false }
+}));
+
+// 路由注册
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
+const loginRoute = require('./routes/login');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/login', loginRoute);
 
-// Export the app instead of listening here
 module.exports = app;
